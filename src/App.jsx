642320260a60edeb534c49dd0224e19041328e9d
fullstackkk <alexsmit17264@gmail.com>
2024-04-16 13:52:08 +0300

    добавили сохранение и удаление карточек в одном столбце

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import "./App.css";
-import { Children, Component, Fragment } from "react";
+import { Component, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-const Card = ({ title, message, tag, user }) => {
+const Card = ({ id, title, message, tag, user, onDelete ,changeCartText }) => {
+  const [inputValue, setInputValue] = useState(title);
+
+  const setValue = (e) => {
+    setInputValue(e.target.value);
+    changeCartText(e.target.value ,id)
+  };
+
   return (
     <div className="card">
-      <h3 className="card__title">{title}</h3>
+      <textarea
+        className="card__title"
+        value={inputValue}
+        onChange={setValue}
+      />
       <div className="card__info">
         <div className="avatar">{user}</div>
         <div>{tag}</div>
+        <button className="delete" onClick={() => onDelete(id)}>
+          delete
+        </button>
       </div>
     </div>
   );
@@ -18,22 +32,84 @@ class Wrapper extends Component {
   constructor(props) {
     super(props);
   }
+  state = {
+    cardList: [],
+  };
+  componentDidMount() {
+    const cards = JSON.parse(localStorage.getItem("to-do")) || [];    
+    this.setState({
+      cardList: cards,
+    });
+  }
+  componentDidUpdate(){
+    localStorage.setItem("to-do", JSON.stringify(this.state.cardList));
+  }
+
+  createHandler = () => {
+    const newCard = {
+      id: Math.ceil(Math.random()*100),
+      title: "...",
+      message: "",
+      tag: "go",
+      user: "A",
+    };
+    this.setState({
+      cardList: [...this.state.cardList, newCard],
+    });
+  };
+
+  onDeleteCard = (id) => {
+    const newCardList = this.state.cardList.filter( item => item.id != id );
+
+    console.log(id)
+    this.setState({
+      cardList: newCardList,
+    });
+  };
+  changeCartText =(text, id)=>{
+
+
+    const newCardList= this.state.cardList.map(item=>{
+      if(item.id === id){
+        return {
+          id: item.id,
+          title: text,
+          message: item.message,
+          tag: item.tag,
+          user: item.user,
+        }
+      }
+      return item
+    })
+
+
+    this.setState({
+      cardList: newCardList,
+    });
+  }
 
   render() {
-    const { type, cards, title } = this.props;
+    const { cardList } = this.state;
+    const { type, title } = this.props;
     const classNames = [type, "colum-wrapper"];
     return (
       <div className={classNames.join(" ")}>
         <h2 className="colum-wrapper__title">{title}</h2>
-        {cards.map((card, index) => (
+        {cardList.map((card, index) => (
           <Card
             title={card.title}
             message={card.message}
             tag={card.tag}
             user={card.user}
-            key={card.title}
+            key={card.id}
+            onDelete={this.onDeleteCard}
+            changeCartText={this.changeCartText}
+            id={card.id}
           />
         ))}
+        <button className="create-card" onClick={this.createHandler}>
+          Create +
+        </button>
       </div>
     );
   }
@@ -48,38 +124,15 @@ PropTypes.shape({
 
 Wrapper.propTypes = {
   type: PropTypes.oneOf(["to-do", "in-progress", "reviev", "done"]).isRequired,
-  cards: PropTypes.array,
 };
 
 function App() {
-  const cards = [
-    {
-      title: "Write code",
-      message: "",
-      tag: "go",
-      user: "A",
-    },
-    {
-      title: "Write code",
-      message: "",
-      tag: "go",
-      user: "A",
-    },
-    {
-      title: "Write code",
-      message: "",
-      tag: "go",
-      user: "A",
-    },
-  ];
+  
   return (
     <div className="app">
       <h1 className="title">Trello</h1>
       <div className="desck">
-        <Wrapper title={"To-do"} type="to-do" cards={cards} />
-        <Wrapper title={"In-Progress"} type="in-progress" cards={cards} />
-        <Wrapper title={"Reviev"} type="reviev" cards={cards} />
-        <Wrapper title={"Done"} type="done" cards={cards} />
+        <Wrapper title={"To-do"} type="to-do" />
       </div>
     </div>
   );
